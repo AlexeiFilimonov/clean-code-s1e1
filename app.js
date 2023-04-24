ac683613f4c64cc665adb6f27c8e12b2c49ad7b9
alexeifilimonov <nlifeaf@mail.ru>
2023-04-24 18:13:13 +0300

    refactor(extended-1.2): add alt attribute for multimedia

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt = 'Delete button';
     deleteButtonImg.className = 'delete-img';
     deleteButton.appendChild(deleteButtonImg);
 
